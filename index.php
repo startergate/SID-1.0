1ffdd77bb02bbcfb591fcad1910b8f6fae6f2864
startergate <32726809+startergate@users.noreply.github.com>
2018-03-19 20:19:13 +0900

    2018-03-19_2 Emergency Fix
    1) Critical Bug Fix.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf=8" />
+	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="apple-touch-icon" sizes="57x57" href="/static/img/favicon/startergate_id/apple-icon-57x57.png">
